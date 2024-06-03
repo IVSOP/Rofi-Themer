12566ca70c3b2e0dfa1e9b69ededd63990cc1aef
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2024-06-03 15:47:00 +0100

    Minor fix

--- src/daemon/main.cpp
+++ src/daemon/main.cpp
@@ -148,7 +148,7 @@ int main (int argc, char **argv) {
 
 	Message msg;
 	while (true) {
-		socklen_t sock_len;
+		socklen_t sock_len = sizeof(addr);
 		int clientSock = accept(sockfd, (struct sockaddr*)&addr, &sock_len);
 		if (clientSock < 0) {
 		    print_error("Error accepting connection\n");
